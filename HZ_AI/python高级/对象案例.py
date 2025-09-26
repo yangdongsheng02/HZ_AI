bc5e087a55e4da077f7093c0741076ca4acd5450
yang-pzz <1151717137@qq.com>
2025-09-27 00:20:57 +0800

    面向对象案例

--- HZ_AI/python高级/对象案例.py
+++ HZ_AI/python高级/对象案例.py
@@ -1,19 +1,54 @@
-class Student:
-    def __init__(self):
-        self.weight = 100
-
-    def run(self):
-        self.weight -= 0.5
-        print(f'跑步一次之后体重为{self.weight}')
-
-    def eat(self):
-        self.weight += 0.5
-        print(f'大吃大喝后体重为{self.weight}')
-
-xiaoming = Student()
-xiaoming.run()
-xiaoming.eat()
+# class Student:
+#     def __init__(self):
+#         self.weight = 100
+#
+#     def run(self):
+#         self.weight -= 0.5
+#         print(f'跑步一次之后体重为{self.weight}')
+#
+#     def eat(self):
+#         self.weight += 0.5
+#         print(f'大吃大喝后体重为{self.weight}')
+#
+# xiaoming = Student()
+# xiaoming.run()
+# xiaoming.eat()
 
 #烤地瓜
+class SweetPotato:
+    #初始化属性
+    def __init__(self):
+        self.cook_time = 0
+        self.cook_state = '生的'
+        self.condi_list = []
 
+    #地瓜被烤
+    def cook(self,time):
+        #被烤时间
+        self.cook_time += time
+        if 0<=self.cook_time<3:
+            self.cook_state = '生的'
+        elif 3<=self.cook_time<7:
+            self.cook_time = '半生不熟'
+        elif 7<=self.cook_time<12:
+            self.cook_time = '熟了'
+        else:
+            self.cook_time = '已烤焦，糊了'
+
+    #添加调料
+    def add_condi(self,condi):
+        self.condi_list.append(condi)
+
+    #信息输出
+    def __str__(self):
+        return f'这个地瓜烤了{self.cook_time},状态是{self.cook_state},添加了{self.condi_list}'
+
+#创建对象
+potato = SweetPotato()
+print(potato)
+
+#调用方法
+potato.cook(3)
+potato.add_condi('蜂蜜')
+print(potato)
 
--- HZ_AI/python高级/魔方方法.py
+++ HZ_AI/python高级/魔方方法.py
@@ -1,20 +1,26 @@
 class Car:
-    def __init__(self,color):
+    def __init__(self,color,number):
         #设置属性 __int__
         self.color = color
+        self.number = number
 
-
-    #     #获取属性:self.属性名
-    # def show(self):
-    #     print(self.color)
-    #     print(self.number)
+        #获取属性:self.属性名
+    def show(self):
+        print(self.color)
+        print(self.number)
 
     #del魔法
     def __del__(self):
         print('自动调用了del魔法方法')
 
-#创建对象:对象名 = 类名()
-car = Car('红色')
+    def __str__(self):
+        print(f'{self.color}的汽车有{self.number}个车轮')
 
-del car
+#创建对象:对象名 = 类名()
+car = Car('红色',4)
+
+# del car #删除对象,文件结束时也会调用
 print(car.color)
+print(car.number)
+print(car.show())
+print(car)
